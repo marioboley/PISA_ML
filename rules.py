59051c952de065b8c3e8640597a5aff30dead489
Wilson <852943830@qq.com>
2021-12-04 22:09:26 +1100

    Change to logloss. Add get_rules function. Fix bugs.

--- rules.py
+++ rules.py
@@ -1,7 +1,10 @@
 from realkd.patch import RuleFit
 from sklearn.model_selection import KFold
 from multilabel import ProbabilisticClassifierChain
-
+import numpy as np
+from sklearn.base import clone
+import re
+from sklearn.metrics import log_loss
 
 class RuleFitWrapper:
 
@@ -17,8 +20,9 @@ class RuleFitWrapper:
         self.rank = []
         self.error = [[] for _ in range(n_splits)]
         self.rank_option = rank
-        self.model = None
+        self.chain = None
         self.rf = None
+        self.num_rules = [[] for _ in range(n_splits)]
         if n_splits < 2: raise Exception ('n_splits should at least 2')
         if rank not in ['median', 'mean']: raise Exception ('Invalid ranking method')
 
@@ -33,53 +37,86 @@ class RuleFitWrapper:
         for train_index, test_index in kf.split(X):
             X_train, X_test = X.iloc[train_index].values, X.iloc[test_index].values
             y_train, y_test = y.iloc[train_index].values, y.iloc[test_index].values
-            if len(y.shape) == 1 or y.shape[1] == 1:
+            if len(y.shape) == 1:
                 # if multiple targets, use chain rules
                 rulefits = [RuleFit(rfmode='classify', model_type='lr', Cs=[C]) for C in self.cs]
             else:
                 rulefits = [ProbabilisticClassifierChain(RuleFit(rfmode='classify', model_type='lr', Cs=[C])) for C in self.cs]
             for each in rulefits:
-                each.fit(X_train, y_train)
-                try:
-                    test_error = sum(sum((y_test - each.predict(X_test))**2))/len(X_test)
-                except:
-                    test_error = sum((y_test - each.predict(X_test))**2)/len(X_test)
                 # SSE
+                # each.fit(X_train, y_train)
+                # try:
+                #     test_error = sum(sum((y_test - each.predict(X_test))**2))/len(X_test)
+                # except:
+                #     test_error = sum((y_test - each.predict(X_test))**2)/len(X_test)
+                each.fit(X_train, y_train)
+                y_pred = each.predict(X_test)
+                test_error = log_loss(y_test, y_pred, eps=1e-15, normalize=True, sample_weight=None, labels=None)
                 self.error[i].append(test_error)
             self.rank.append([sorted(self.error[i]).index(l) for l in self.error[i]])
             i += 1
-        # print(self.rank)
-        # print(self.error)
         if self.rank_option =='median':
                 self.rank = np.median(np.array(self.rank), axis=0)
         else:
             self.rank = np.mean(np.array(self.rank), axis=0)
-        lst = list(self.rank)   
+        lst = list(self.rank)
         indx = lst.index(min(lst))
-        self.rf = RuleFit(rfmode='classify', model_type='lr', Cs=[self.cs[indx]])
-        if len(y.shape) == 1 or y.shape[1] == 1:
-            self.model = self.rf
+        rf = RuleFit(rfmode='classify', model_type='r', Cs=[self.cs[indx]])
+        if len(y.shape) == 1:
+            # self.chain = rf
+            self.rf = clone(rf)
+            self.rf.fit(X.values, y.values) # change to matrix
         else:
-            self.model = ProbabilisticClassifierChain(self.rf)
-        self.model.fit(X,y)
+            self.chain = ProbabilisticClassifierChain(clone(rf))
+            self.chain.fit(X,y)
+        return self
 
     def predict_proba(self, X):
         """This is only for predicting probability of p(y=1).
         """
-        return self.model.predict_proba(X)
+        if self.chain:
+            return self.chain.predict_proba(X)
+        return self.rf.predict_proba(X.values)
 
     def predict(self, X):
         """This is for converting probability to binary (0, 1)
         if p>0.5, label=1, otherwise 0
         """
-        result = self.predict_proba(X)
-        result[result>=0.5] = 1
-        result[result<0.5] = 0
+        if self.chain:
+            result = self.predict_proba(X)
+            result[result>=0.5] = 1
+            result[result<0.5] = 0
+        else:
+            result = self.rf.predict(X.values)
         return result
 
-    def get_rules(self):
-        """Get positive rules
+    def format_rules(self, feature_names, data):
+        """This is for format the rule features
+        Input: feature_names: feature names
+               data: dataframe
         """
-        rules = self.rf.get_rules()
-        rules = rules[rules['coef'] > 0]
-        return rules.iloc[np.argsort(rules['importance'])][::-1]
+        dic_rules = dict(zip(['feature_' + str(i) + ' ' for i in range(len(feature_names))], feature_names))
+        for key in dic_rules:
+            if re.match(r'(.*?)' + key, data):
+                data = re.sub(key, dic_rules[key], data)
+        return data
+
+    def get_rules(self, X, y):
+        """Get positive rules
+        Input: X: features
+               y: targets
+        """
+        y_col = y.columns.tolist()
+        sum_columns = X.columns.tolist() + y_col
+        res = {}
+        if self.chain:
+            rf = self.chain.fitted_
+        else:
+            rf = [self.rf]
+        for i, est in enumerate(rf):
+            rules = est.get_rules()
+            rules = rules[rules['coef'] > 0]
+            rules.iloc[np.argsort(rules['importance'])][::-1]
+            rules['rule'] = rules.apply(lambda x: self.format_rules(sum_columns, x.rule), axis=1)
+            res[y_col[i]] = rules
+        return res
