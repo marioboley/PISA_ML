f6fc21df7fae70e6b65407965ea9e1198bb330dd
Mario Boley <mario.boley@gmail.com>
2022-06-16 16:41:29 +1000

    central specification for all models to evaluate

---
+++ models.py
@@ -0,0 +1,18 @@
+"""
+Specifies centrally all models used in this study.
+"""
+
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import ClassifierChain
+from multilabel import ProbabilisticClassifierChain
+
+import numpy as np
+
+STATE = np.random.RandomState(seed=1000)
+
+linear_base = LogisticRegressionCV(penalty='l2', solver='lbfgs', random_state=STATE)
+linear_pcc = ProbabilisticClassifierChain(linear_base) 
+
+random_forest_base = RandomForestClassifier(random_state=STATE, min_samples_leaf=1, n_estimators=100)
+random_forest_pcc = ProbabilisticClassifierChain(random_forest_base)
