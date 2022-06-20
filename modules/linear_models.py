a53319fab725edea51c8fd4151d3ea37f4c55899
YIWEN LU <852943830@qq.com>
2022-06-20 12:33:02 +1000

    Fix add_constant

--- modules/linear_models.py
+++ modules/linear_models.py
@@ -22,7 +22,7 @@ class GlmWrapper:
 
     def fit(self, x, y):
         if self.intercept:
-            x = sm.add_constant(x)
+            x = sm.add_constant(x, has_constant='add')
         self.model = sm.GLM(y, x, family=sm.families.Binomial())
         self.fitted = self.model.fit_regularized(alpha=self.alpha, maxiter=self.maxiter, L1_wt=self.L1_wt) if self.penalty else self.model.fit()
         return self
@@ -31,7 +31,7 @@ class GlmWrapper:
         """This is only for predicting probability of p(y=1).
         """
         if self.intercept:
-            x = sm.add_constant(x)
+            x = sm.add_constant(x, has_constant='add')
         return np.column_stack((1-self.fitted.predict(x), self.fitted.predict(x)))
 
     def predict(self, x):
@@ -39,7 +39,7 @@ class GlmWrapper:
         if p>0.5, label=1, otherwise 0
         """
         if self.intercept:
-            x = sm.add_constant(x)
+            x = sm.add_constant(x, has_constant='add')
         return np.where(self.fitted.predict(x)>=0.5, 1, 0)
 
 class GlmWrapperCV(GlmWrapper):
