610549b7477c6c20764d51e9fc1ed4f515538ab2
YIWEN LU <852943830@qq.com>
2022-06-28 17:35:36 +1000

    Remove unused model

--- models.py
+++ models.py
@@ -24,27 +24,11 @@ random_forest_ind = BinaryRelevanceClassifier(random_forest_base)
 
 # Here are the final models
 # GAM models
-# gam_base = GamWrapper(n_splines=20, spline_order=5, max_iter=MAX_ITER) # these gam models are not working
-# gam_pcc = ProbabilisticClassifierChain(gam_base)
 gam_base = ExplainableBoostingClassifier(random_state=seeds) # not support numpy
 gam_pcc = ProbabilisticClassifierChain(gam_base)
 
 # rule fit models
-rule_fit_base = RuleFitWrapperCV(Cs = [1, 2, 4, 8, 16, 32], cv=5, rank='median', random_state=STATE)
+rule_fit_base = RuleFitWrapperCV(Cs = [1, 2, 4, 8, 16, 32], cv=10, rank='median', random_state=STATE)
 rule_fit_pcc = ProbabilisticClassifierChain(rule_fit_base)
 
-rule_fit_base_mean = RuleFitWrapperCV(Cs = [1, 2, 4, 8, 16, 32], cv=5, rank='mean', random_state=STATE)
-rule_fit_pcc_mean = ProbabilisticClassifierChain(rule_fit_base_mean)
 
-# Here are the NON-USE models
-linear_non_base = LogisticRegression(penalty='none', max_iter=MAX_ITER)
-glm_non_base = GlmWrapper(intercept=True, penalty=False, max_iter=MAX_ITER)
-glm_non_pcc = ProbabilisticClassifierChain(glm_non_base)
-linear_non_pcc = ProbabilisticClassifierChain(linear_non_base)
-
-linear_l1_base = LogisticRegressionCV(penalty='l1', solver='saga', random_state=STATE, max_iter=MAX_ITER)
-glm_l1_base = GlmWrapper(intercept=True, penalty=True, max_iter=MAX_ITER, alpha=5, L1_wt=1)
-glm_l2_base = GlmWrapper(intercept=True, penalty=True, max_iter=MAX_ITER, alpha=5, L1_wt=0)
-linear_l1_pcc = ProbabilisticClassifierChain(linear_l1_base)
-glm_l1_pcc = ProbabilisticClassifierChain(glm_l1_base)
-glm_l2_pcc = ProbabilisticClassifierChain(glm_l2_base)
