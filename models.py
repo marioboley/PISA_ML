b69701b404afd77f6d2ed63fc7b9fb73578aab9d
YIWEN LU <852943830@qq.com>
2022-06-29 15:01:30 +1000

    Update labels to match the paper.
    GAM still holds interaction terms

--- models.py
+++ models.py
@@ -4,27 +4,27 @@ Specifies centrally all models used in this study.
 
 from sklearn.ensemble import RandomForestClassifier
 from modules.multilabel import ProbabilisticClassifierChain, BinaryRelevanceClassifier
-from sklearn.linear_model import LogisticRegressionCV, LogisticRegression
+from sklearn.linear_model import LogisticRegressionCV
 from modules.rules import RuleFitWrapperCV
-from modules.linear_models import GlmWrapper
-from modules.gam import GamWrapper
 from interpret.glassbox import ExplainableBoostingClassifier
 import numpy as np
 
+import data
 seeds = 1000
 STATE = np.random.RandomState(seed=seeds)
 
 MAX_ITER = 30000
+# logistic models with l2
 linear_base = LogisticRegressionCV(penalty='l2', solver='lbfgs', random_state=STATE, max_iter=MAX_ITER)
 linear_pcc = ProbabilisticClassifierChain(linear_base) 
 
+# Random Forest
 random_forest_base = RandomForestClassifier(random_state=STATE, min_samples_leaf=1, n_estimators=200)
 random_forest_pcc = ProbabilisticClassifierChain(random_forest_base)
 random_forest_ind = BinaryRelevanceClassifier(random_forest_base)
 
-# Here are the final models
 # GAM models
-gam_base = ExplainableBoostingClassifier(random_state=seeds) # not support numpy
+gam_base = ExplainableBoostingClassifier(interactions=0,random_state=seeds) # not support numpy
 gam_pcc = ProbabilisticClassifierChain(gam_base)
 
 # rule fit models
