5438e1bcadef8d39fbc7aa0a87e43042736e8744
YIWEN LU <852943830@qq.com>
2022-06-29 12:34:47 +1000

    remove unused variable definition

--- common.py
+++ common.py
@@ -8,18 +8,3 @@ import os
 PROJECT_ROOT_DIR = "."
 DATAPATH = os.path.join(PROJECT_ROOT_DIR, "data")
 OUTPUTPATH = os.path.join(PROJECT_ROOT_DIR, "output")
-
-# x = data.x
-# x1 = data.x1
-# y = data.y
-# target = data.targets
-# sphere = data.sphere
-# worm = data.worm
-# vesicle = data.vesicle
-# other = data.other
-# comp_ids = data.comp_ids
-# polymers = data.polymers
-# predictors = data.predictors
-# corona_comp = data.corona_comp
-# core_comp = data.core_comp
-
