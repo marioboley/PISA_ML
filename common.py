3234b52f149953b6a340e7cd5fddac1edaa85842
YIWEN LU <852943830@qq.com>
2022-12-01 17:09:23 +1100

    Add ignore warning function Only ignore missing feature names warning

--- common.py
+++ common.py
@@ -4,12 +4,12 @@ all code is run from the directory of this module as working directory.
 """
 
 import os
-import numpy as np
 import warnings
 
 PROJECT_ROOT_DIR = "."
 DATAPATH = os.path.join(PROJECT_ROOT_DIR, "data")
 OUTPUTPATH = os.path.join(PROJECT_ROOT_DIR, "output")
 
-def ignore_warning():
-    warnings.filterwarnings('ignore', message='X has feature names, but *')
+def ignore_warning(ignore_feature_name_warning=True):
+    if ignore_feature_name_warning:
+        warnings.filterwarnings('ignore', message='X has feature names, but *')
